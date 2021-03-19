eafc21128d84a721f7e8e58fd31cb06a1b27f75b
Zubair Khan <zubair.khan6273@gmail.com>
2021-03-19 23:23:39 +0500

    Added tentative horizontal movement on keyboard

---
+++ GroupSix/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float walkSpeed= 1f;
+    [SerializeField] Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalVelocity = Input.GetAxis("Horizontal") * walkSpeed;
+        rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+    }
+}
