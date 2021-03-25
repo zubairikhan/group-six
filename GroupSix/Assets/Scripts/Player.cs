a86deeed52cd47c60f67f4de28187cea0e210e5e
emmvi <maryam.altaf.10@gmail.com>
2021-03-25 05:03:34 +0500

    walking animation triggered with code

--- GroupSix/Assets/Scripts/Player.cs
+++ GroupSix/Assets/Scripts/Player.cs
@@ -11,66 +11,117 @@ public class Player : MonoBehaviour
     bool isWalking = false;
     bool jumped = false;
     [SerializeField] Rigidbody2D rb;
+
+    private Animator anim;
+    private float dirX;
+    private bool facingRight = true;
+    private Vector3 localScale;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        localScale = transform.localScale;
+                
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxis("Horizontal")!=0)
-        {
-            isWalking = true;
-        }
-        else
-        { 
-            isWalking = false;
+        dirX = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        if(Input.GetButtonDown("Jump") && rb.velocity.y == 0){
+            rb.AddForce(Vector2.up * 500f);
         }
 
-        //Walk();
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            jumped = true;
+        if(Mathf.Abs(dirX) > 0 && rb.velocity.y == 0){
+            anim.SetBool("isWalking", true);
         }
-    }
-
-    private void FixedUpdate()
-    {
-        if (isWalking)
-        {
-            Walk();
+        else{
+            anim.SetBool("isWalking", false);
         }
-        if (jumped)
-        {
-            Jump();
+
+        if(rb.velocity.y == 0){
+            anim.SetBool("jumped", false);
         }
-        if ( rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; 
+        if (rb.velocity.y > 0){
+            anim.SetBool("jumped", true);
         }
-    }
-
-    private void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-        jumped = false;
-    }
-
-    private void Walk()
-    {
-        /*var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * walkSpeed;
-        var newXPos = transform.position.x + deltaX;
-        transform.position = new Vector2(newXPos, transform.position.y);
-        */
-
-        float horizontalVelocity = Input.GetAxis("Horizontal") * walkSpeed;
-        rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+        if (rb.velocity.y < 0){
+            anim.SetBool("jumped", false);
+        }
         
 
-        //rb.AddForce(new Vector2(Input.GetAxis("Horizontal")* walkSpeed, 0));
+    //     if (Input.GetAxis("Horizontal")!=0)
+    //     {
+    //         isWalking = true;
+    //     }
+    //     else
+    //     { 
+    //         isWalking = false;
+    //     }
+
+    //     //Walk();
+    //     if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+    //     {
+    //         jumped = true;
+    //     }
+    // }
     }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+        
+        // if (isWalking)
+        // {
+        //     Walk();
+        // }
+        // if (jumped)
+        // {
+        //     Jump();
+        // }
+        // if ( rb.velocity.y < 0)
+        // {
+        //     rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; 
+        // }
+    }
+
+    void LateUpdate(){
+        if (dirX > 0){
+            facingRight = true;
+        }
+        else if(dirX < 0){
+            facingRight = false;
+        }
+
+        if(((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0))){
+            localScale.x *= -1;
+        }
+        transform.localScale = localScale;
+    }
+    
+
+    // private void Jump()
+    // {
+    //     rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+    //     jumped = false;
+    // }
+
+    // private void Walk()
+    // {
+    //     /*var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * walkSpeed;
+    //     var newXPos = transform.position.x + deltaX;
+    //     transform.position = new Vector2(newXPos, transform.position.y);
+    //     */
+
+    //     float horizontalVelocity = Input.GetAxis("Horizontal") * walkSpeed;
+    //     rb.velocity = new Vector2(horizontalVelocity, rb.velocity.y);
+        
+
+    //     //rb.AddForce(new Vector2(Input.GetAxis("Horizontal")* walkSpeed, 0));
+    // }
 
     public void SetGroundedStatus(bool status)
     {
