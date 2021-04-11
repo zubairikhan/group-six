0be801bc3446bf1d5c16c452b124f26c3ca6e906
hudailyas <huda.milyas@gmail.com>
2021-04-11 15:57:27 +0400

    enemy movement added

---
+++ GroupSix/Assets/enemy1move.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy1move : MonoBehaviour
+{
+    public float speed;
+    public bool MoveRight;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if move right bool is true meaning enemy will move to the right
+        if (MoveRight){
+            transform.Translate(2 * Time.deltaTime * speed,0,0);
+            transform.localScale = new Vector2(2,2);
+
+        }
+        else{
+            transform.Translate(-2 * Time.deltaTime * speed,0,0);
+            transform.localScale = new Vector2(-2,2);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig){
+        if(trig.gameObject.CompareTag("turn")){
+            if (MoveRight){
+                MoveRight = false;
+            }
+            else{
+                MoveRight= true;
+                
+            }
+        }
+    }
+}
