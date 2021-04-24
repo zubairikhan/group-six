7a13f3ac65db062912b3cbb69fcff65d3b1f7581
Zubair Khan <zubair.khan6273@gmail.com>
2021-04-24 21:53:16 +0500

    Update EnemyTop.cs. KillEnemy function

--- GroupSix/Assets/Scripts/EnemyTop.cs
+++ GroupSix/Assets/Scripts/EnemyTop.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class EnemyTop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Kill enemy");
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(transform.parent.gameObject);
+            KillEnemy();
         }
+    }
+
+    private void KillEnemy()
+    {
+        Destroy(transform.parent.gameObject);
     }
 }
