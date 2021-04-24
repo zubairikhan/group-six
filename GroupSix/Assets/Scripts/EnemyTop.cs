3b987d54f01b32a50bd6c1cadaf18007fbdbf31d
rizwan19947 <expectp@gmail.com>
2021-04-25 04:30:18 +0500

    Battery spawning as a loot from enemies

--- GroupSix/Assets/Scripts/EnemyTop.cs
+++ GroupSix/Assets/Scripts/EnemyTop.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class EnemyTop : MonoBehaviour
 {
+
+
+    public GameObject lootBattery;
+    BatteryRotation obj; 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             KillEnemy();
+
+
+            
+            Instantiate(lootBattery, transform.parent.gameObject.transform.position, Quaternion.identity);
         }
     }
 
