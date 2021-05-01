eec1a9db273b40837545a3e4f05391c66edbd410
Zubair Khan <zubair.khan6273@gmail.com>
2021-05-01 18:11:08 +0500

    Created updraft button and activated updraft region through it

---
+++ GroupSix/Assets/Scripts/UpdraftStarterButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdraftStarterButton : MonoBehaviour
+{
+    [SerializeField] GameObject updraft;
+    [SerializeField] Animator animator;
+
+    bool started= false;
+    
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Stomper")
+        {
+            if (!started)
+            {
+                ActivateUpdraft();
+            }
+            
+        }
+    }
+
+    private void ActivateUpdraft()
+    {
+        Debug.Log("updraft activated");
+        animator.SetBool("buttonPushed", true);
+        started = true;
+        updraft.SetActive(true);
+    }
+
+}
