026de399b685d2bce0fdd79f9803fd7410e7cce2
Zubair Khan <zubair.khan6273@gmail.com>
2021-05-01 16:51:04 +0500

    created switch board

--- GroupSix/Assets/Scripts/Switch.cs
+++ GroupSix/Assets/Scripts/Switch.cs
@@ -6,11 +6,14 @@ public class Switch : MonoBehaviour
 {
     [SerializeField] BoxCollider2D boxCollider;
     [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer light;
     
 
     public void Activate()
     {
+        light.color = new Color(0, 1, 0, 1);
         animator.SetBool("open", true);
+        
         Debug.Log("Switch activated");
         boxCollider.enabled = false;
     }
