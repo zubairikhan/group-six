5b6daaa30cc16916710e6842da9664d14a439c0b
Zubair Khan <zubair.khan6273@gmail.com>
2021-05-01 16:35:59 +0500

    added door that opens from switch using cell

--- GroupSix/Assets/Scripts/Switch.cs
+++ GroupSix/Assets/Scripts/Switch.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Switch : MonoBehaviour
 {
     [SerializeField] BoxCollider2D boxCollider;
+    [SerializeField] Animator animator;
     
 
     public void Activate()
     {
+        animator.SetBool("open", true);
         Debug.Log("Switch activated");
         boxCollider.enabled = false;
     }
