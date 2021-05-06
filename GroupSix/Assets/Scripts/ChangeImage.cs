c0424f35fd005a55ed808b92d1506d8a7566f8be
hudailyas <huda.milyas@gmail.com>
2021-05-06 09:21:06 +0500

    door audio + remaining buttons

--- GroupSix/Assets/Scripts/ChangeImage.cs
+++ GroupSix/Assets/Scripts/ChangeImage.cs
@@ -6,27 +6,53 @@ using UnityEngine.UI;
 public class ChangeImage : MonoBehaviour
 {
     [SerializeField]
-    //private GameObject bulb; 
-    //private SpriteRenderer bulbColor; 
+     
 
     private int State; // 0 - Off, 1 - On 
     [SerializeField]
 
     private Sprite[] switchSprites; 
     private Image switchImage; 
+    private bool muted = false;
 
     void Start()
     {
-        //bulbColor = bulb.GetComponent<SpriteRenderer>();
-        State = 0; // bulb is Off at the beginning 
+        if (PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else 
+        {
+            Load();
+        }
+        
+        State = 0; // sound is Off at the beginning 
         switchImage = GetComponent<Button>().image;
         switchImage. sprite = switchSprites[State]; 
         gameObject.GetComponent<Button>().onClick.AddListener(TurnOnAndOff); 
+        AudioListener.pause = muted;
     }
 
     private void TurnOnAndOff()
     {
+        if(muted == false){
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else{
+            muted = false;
+            AudioListener.pause = false;
+        }
+
         State = 1 - State;
         switchImage.sprite = switchSprites[State];
+        Save();
+    }
+    private void Load(){
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+    private void Save(){
+        PlayerPrefs.SetInt("muted",muted? 1:0);
     }
 }
