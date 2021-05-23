016c8ba653d5b681db4616f4e6655be7deb5c9e0
hudailyas <huda.milyas@gmail.com>
2021-05-23 13:15:49 +0500

    enemy movements

--- GroupSix/Assets/Scripts/Enemy2move.cs
+++ GroupSix/Assets/Scripts/Enemy2move.cs
@@ -2,95 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy2move : MonoBehaviour
+public class Enemy2move : enemy1move
 {
-    public float speed, range, timeBTWShots, shootSpeed;
-    //public bool MoveRight;
-    public float distToPlayer;
+    // public float speed, range, timeBTWShots, shootSpeed;
+    // //public bool MoveRight;
+    // public float distToPlayer;
     
 
-    public Rigidbody2D rb;
-    public Transform groundCheckPos;
-    public LayerMask groundLayer;
-    public Collider2D bodyCollider;
-    public Transform player, shootPos;
-    public GameObject bullet;
-
-    //[HideInInspector]
-    public bool mustPatrol;
-    public bool mustTurn, canShoot;
+    // public Rigidbody2D rb;
+    // public Transform groundCheckPos;
+    // public LayerMask groundLayer;
+    // public Collider2D bodyCollider;
+    // public Transform player, shootPos;
     
 
-    void Start()
+    // //[HideInInspector]
+    // public bool mustPatrol;
+    // public bool mustTurn;
+     public GameObject bullet;
+     public float shootSpeed, timeBTWShots;
+     public bool canShoot;
+     public Transform shootPos;
+    
+
+    public override void Start()
     {
-
-        rb = GetComponent<Rigidbody2D>(); 
-        mustPatrol = true;
+        base.Start();
         canShoot = true;
     }
 
-    void Update()
+    public override void Update()
     {
-        if(mustPatrol){
-            Patrol();
-        }
-
-        distToPlayer = Vector2.Distance(transform.position, player.position);
-        
+        base.Update();       
         if(distToPlayer <= range)
         {
-            
-            if(player.position.x > transform.position.x && transform.localScale.x < 0
-            ||player.position.x < transform.position.x && transform.localScale.x > 0)
-            {
-                Flip();
-            }
-
-            mustPatrol = false;
-            rb.velocity = Vector2.zero;
-
+          
             if(canShoot)
             {
-                
                 StartCoroutine(Shoot());
             }
+
         }
-        else
-        {
-            mustPatrol = true;
+      
+    }
+    public override void OnTriggerEnter2D(Collider2D trig){
+        base.OnTriggerEnter2D(trig);  
         }
-    }
-
-    // private void FixedUpdate()
-    // {
-    //     if(mustPatrol)
-    //     {
-    //         mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
-    //     }
-    // }
-
-    void Patrol()
-    {
-        // if(mustTurn || bodyCollider.IsTouchingLayers(groundLayer))
-        // {
-        //     Flip();
-        // }
-        transform.Translate(speed * Time.deltaTime * 2,0,0);
-        //rb.velocity = new Vector2(speed * Time.fixedDeltaTime, rb.velocity.y);
-    }
-
-    void OnTriggerEnter2D(Collider2D trig){
-        if(trig.gameObject.CompareTag("turn")){
-         Flip();   
-        }
-    }
-
-    public void Flip(){
-        mustPatrol = false;
-        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        speed *= -1;
-        mustPatrol = true;
-    }
 
     IEnumerator Shoot(){ 
         canShoot = false;
--- GroupSix/Assets/Scripts/enemy1move.cs
+++ GroupSix/Assets/Scripts/enemy1move.cs
@@ -4,41 +4,87 @@ using UnityEngine;
 
 public class enemy1move : MonoBehaviour
 {
-    public float speed;
-    public bool MoveRight;
-  
-    
+     public float speed, range;
+    //public bool MoveRight;
+    public float distToPlayer;
     
 
-    // Update is called once per frame
-    void Update()
+    public Rigidbody2D rb;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+    public Transform player;
+   
+
+    //[HideInInspector]
+    public bool mustPatrol;
+    public bool mustTurn;
+    
+
+    public virtual void Start()
     {
-        //if move right bool is true meaning enemy will move to the right
-        if (MoveRight){
-            transform.Translate(2 * Time.deltaTime * speed,0,0,Camera.main.transform);
-            transform.localScale = new Vector2(2,2);
 
-        }
-        else{
-            transform.Translate(-2 * Time.deltaTime * speed,0,0,Camera.main.transform);
-            transform.localScale = new Vector2(-2,2);
-        }
-        
+        rb = GetComponent<Rigidbody2D>(); 
+        mustPatrol = true;
     }
-    
 
-    
+    public virtual void Update()
+    {
+         if(mustPatrol){
+            Patrol();
+         }
 
-    void OnTriggerEnter2D(Collider2D trig){
-        if(trig.gameObject.CompareTag("turn")){
-            if (MoveRight){
-                MoveRight = false;
+        distToPlayer = Vector2.Distance(transform.position, player.position);
+        
+        if(distToPlayer <= range)
+        {
+            
+            if(player.position.x > transform.position.x && transform.localScale.x < 0
+            ||player.position.x < transform.position.x && transform.localScale.x > 0)
+            {
+                Flip();
             }
-            else{
-                MoveRight= true;
-                
-            }
+
+           mustPatrol = false;
+         
+        rb.velocity = Vector2.zero;
+
+       
         }
+          else{
+            mustPatrol = true;
+        }
+      
+    }
+
+    // private void FixedUpdate()
+    // {
+    //     if(mustPatrol)
+    //     {
+    //         mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+    //     }
+    // }
+
+    void Patrol()
+    {
+        // if(mustTurn || bodyCollider.IsTouchingLayers(groundLayer))
+        // {
+        //     Flip();
+        // }
+        transform.Translate(speed * Time.deltaTime * 2,0,0);
+        //rb.velocity = new Vector2(speed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    public virtual void OnTriggerEnter2D(Collider2D trig){
+        if(trig.gameObject.CompareTag("turn")){
+         Flip();   
+        }
+    }
+      public void Flip(){
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        speed *= -1;
+        mustPatrol = true;
     }
 
    
