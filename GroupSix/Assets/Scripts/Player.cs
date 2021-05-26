33cd8bbe9b10e59dd056285c6c65791f057a59cd
Zubair Khan <zubair.khan6273@gmail.com>
2021-05-26 21:58:02 +0500

    converted to mobile inputs

--- GroupSix/Assets/Scripts/Player.cs
+++ GroupSix/Assets/Scripts/Player.cs
@@ -107,10 +107,10 @@ public class Player : MonoBehaviour
        
 
         //movement with keyboard
-        dirX = Input.GetAxisRaw("Horizontal");
+        //dirX = Input.GetAxisRaw("Horizontal");
 
         //////////////////////////////////////////////////////////////////////For joystick control. Do NOT delete///////////////////////////////////////////////////////////////////////////////
-        /*
+        
         if (joystick.Horizontal >= 0.5f)
         {
             dirX = 1;
@@ -123,7 +123,7 @@ public class Player : MonoBehaviour
         {
             dirX = 0f;
         }
-        */
+        
 
 
 
@@ -144,7 +144,7 @@ public class Player : MonoBehaviour
 
 
         //jump using keyboard
-        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && extraJumpsLeft > 0)
+        /*if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && extraJumpsLeft > 0)
         {
             jump = true;
         }
@@ -157,12 +157,11 @@ public class Player : MonoBehaviour
         if((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && canStomp)
         {
             ToggleStompMode(true);
-            
-            
         }
+        */
 
         //stomping using mobile
-        /*
+        
         if (joystick.Vertical > -0.2f)
         {
             swipedDown = false;
@@ -176,14 +175,14 @@ public class Player : MonoBehaviour
                 swipedDown = true;
             }
         }
-        */
+        
 
 
 
 
         //Action button(switch+deflect) using keyboard
 
-        if (Input.GetKeyDown(KeyCode.G) && nearToSwitch)
+        /*if (Input.GetKeyDown(KeyCode.G) && nearToSwitch)
         {
             if(scoreScript.GetCellCount() > 0)
             {
@@ -195,6 +194,7 @@ public class Player : MonoBehaviour
         {
             enemyProjectileRb.velocity = -enemyProjectileRb.velocity;
         }
+        */
         
 
 
@@ -230,12 +230,13 @@ public class Player : MonoBehaviour
         Walk();
 
         //jump using keyboard
-        if (jump)
+        /*if (jump)
         {
             Jump();
             
             
         }
+        */
         
         
         
@@ -289,24 +290,24 @@ public class Player : MonoBehaviour
     {
         //Debug.Log("jump pressed");
         //Jump for mobile input
-        /*if(extraJumpsLeft > 0 || extraJumpsLeft <=0 && isGrounded)
+        if(extraJumpsLeft > 0 || extraJumpsLeft <=0 && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             extraJumpsLeft--;
             anim.SetBool("jumped", true);
             //jump = false;
         }
-        */
+        
         
         
         //jump using keyboard
-        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        /*rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
         extraJumpsLeft--;
         audioManager.Play("jump grunt");
         anim.SetBool("jumped", true);
         jump = false; 
         //anim.SetBool("isStomping", false);
-        
+        */
 
 
         StartCoroutine(ToggleStompPermission());
